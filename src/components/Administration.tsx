0f1b7dbfc0a7ad059bac0b64fd0690aa8d55adc8
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-11 08:39:15 +0100

    Create first components

---
+++ src/components/Administration.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './../App.css';
+
+const Administration = () => {
+    return (
+        <div className="App">
+            <h2>Administration</h2>
+        </div>
+    )
+}
+
+export default Administration;
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../App.css';
+
+const Dashboard = () => {
+    return (
+        <div className="App">
+            <h2>Dashboard</h2>
+        </div>
+    )
+}
+
+export default Dashboard;
