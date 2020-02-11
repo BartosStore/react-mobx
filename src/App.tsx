f9afccbc17b251186cd19ba137a14a22b2fb49c5
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-11 13:23:54 +0100

    Create dashboard section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
 import AdministrationPage from "./components/administration/AdministrationPage";
 import './App.css';
 
