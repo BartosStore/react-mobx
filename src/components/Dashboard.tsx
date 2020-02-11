a473b70d4c84eba86256de0db93734654d725c89
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-11 12:58:43 +0100

    Render simple chart

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import BarChartData from "./BarChartData";
 import './../App.css';
 
 const Dashboard = () => {
     return (
         <div className="App">
             <h2>Dashboard</h2>
+            <div className="Center">
+                <BarChartData />
+            </div>
         </div>
     )
 }
