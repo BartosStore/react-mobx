5e7915075da8b7955a40d4efcc14e4f15855d0cf
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-11 09:44:48 +0100

    Display value from store

--- src/components/administration/AdministrationPage.tsx
+++ src/components/administration/AdministrationPage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './../../App.css';
 
+class AdministrationStore {
+    temperature: number = 20;
+
+    get temp() {
+        return this.temperature;
+    }
+}
+
+const administrationStore = new AdministrationStore();
+
 const AdministrationPage = () => {
     return (
         <div className="App">
             <h2>Administration</h2>
+            <p>Temperature: {administrationStore.temp}</p>
         </div>
     )
 }
