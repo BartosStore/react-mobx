ca66d93e33507f6b733090b3da0c5698e0f1030f
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-11 09:16:09 +0100

    Rename file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import AdministrationPage from "./components/administration/Administration";
+import AdministrationPage from "./components/administration/AdministrationPage";
 import './App.css';
 
 const App = () => {
