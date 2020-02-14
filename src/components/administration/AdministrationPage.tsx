e9b0d5bf366d5fb575e5d9e5eae28289a3cf75de
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-14 10:50:31 +0100

    Style administration with reactstrap

--- src/components/administration/AdministrationPage.tsx
+++ src/components/administration/AdministrationPage.tsx
@@ -2,22 +2,37 @@ import React from "react";
 import { Observer } from "mobx-react"
 
 import { administrationStore } from "./AdministrationStore";
-import { Button } from "reactstrap";
-// import './../../App.css';
+import { Button, Table, Container, Row, Col } from "reactstrap";
 
 const store = administrationStore;
 
 const AdministrationPage = () => {
-    // const store = new AdministrationStore();
     return (
-        <div className="App">
-            <h2>Administration</h2>
-            <div className="Form">
-                <Observer>{() => <div>{store.temp}</div>}</Observer>
-                <Button type="submit" color="success" onClick={() => store.increaseTemp()}>Increase</Button>
-                <Button type="submit" color="danger" onClick={() => store.decreaseTemp()}>Decrease</Button>
-            </div>
-        </div>
+        <Container >
+            <Row>
+                <Col sm="12" md={{ size: 6, offset: 3 }} lg={{ size: 6, offset: 3 }} xl={{ size: 6, offset: 3 }}>
+                    <h1>Administration</h1>
+                    <Table>
+                        <tbody>
+                            <tr>
+                                <th>Min temperature</th>
+                                <td>16.0</td>
+                            </tr>
+                            <tr>
+                                <th>Max temperature</th>
+                                <td>30.0</td>
+                            </tr>
+                            <tr>
+                                <th>Actual temperature</th>
+                                <td><Observer>{() => <div>{store.temp}</div>}</Observer></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    <Button type="submit" color="success" onClick={() => store.increaseTemp()}>Increase</Button>
+                    <Button type="submit" color="danger" onClick={() => store.decreaseTemp()}>Decrease</Button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
