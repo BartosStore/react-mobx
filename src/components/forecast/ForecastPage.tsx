c110788c319a6f3af5cd2449596c1e0f7fcab9d8
Miroslav Bartoš <mirek-b@seznam.cz>
2020-02-18 14:29:11 +0100

    Fetch weather data

--- src/components/forecast/ForecastPage.tsx
+++ src/components/forecast/ForecastPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { owmUrl } from '../../res/constants';
 
 const ForecastPage = () => {
+  const [temp, setTemp] = useState('');
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    fetch(owmUrl).then((response: any) => {
+      response.json().then((weather: any) => {
+        setTemp(weather.main.temp);
+        setCity(weather.name + ', ' + weather.sys.country);
+      });
+    });
+  })
+
   return (
     <div className="App">
       <h2>Forecast</h2>
       <h3>Current weather</h3>
-      <div>Temperature: 22.0</div>
+      <div>City: {city}</div>
+      <div>Temperature: {temp} °C</div>
     </div>
   )
 }
--- src/res/constants.ts
+++ src/res/constants.ts
@@ -4,3 +4,5 @@ export const ROUTES = {
     FORECAST: '/forecast',
     ADMIN: '/admin',
 }
+
+export const owmUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Prague&units=metric&appid=d4d5148072fe1ce3511bd48179421787';
